f46fe4ea58d58cb768364c33a54489b845ef95c6
Tom Heffels <tomheffels@hotmail.com>
2019-05-25 00:25:14 +0200

    created endpoint that prints message in text prop

--- messages-api.js
+++ messages-api.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
-app.listen(3000, () => console.log(`Express API listening on port ${port}`))
+app.use(bodyParser.json())
+
+app.post('/messages', (req, res) => {
+  !req.body.text ? res.status(400) : (console.log('Message: ', req.body.text),
+  res.json({message: `${req.body.text}`}))
+})
+
+app.listen(port, () => console.log(`Express API listening on port ${port}`))
