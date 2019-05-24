3da9516191814d81acd0bbac0f43522e08fbc893
Tom Heffels <tomheffels@hotmail.com>
2019-05-25 00:26:29 +0200

    .

--- messages-api.js
+++ messages-api.js
@@ -7,8 +7,11 @@ const port = 3000
 app.use(bodyParser.json())
 
 app.post('/messages', (req, res) => {
-  !req.body.text ? res.status(400) : (console.log('Message: ', req.body.text),
-  res.json({message: `${req.body.text}`}))
+  !req.body.text ? 
+    res.status(400) : (
+    console.log('Message: ', req.body.text),
+    res.json({message: `${req.body.text}`})
+    )
 })
 
 app.listen(port, () => console.log(`Express API listening on port ${port}`))
