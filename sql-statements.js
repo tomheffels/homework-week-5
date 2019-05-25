1564f4690d71b9a3c3c22f586f3f0c79ad3f21af
Tom Heffels <tomheffels@hotmail.com>
2019-05-25 02:36:23 +0200

    update/select queries, logs

--- sql-statements.js
+++ sql-statements.js
@@ -1,15 +1,18 @@
 const {Pool} = require('pg')
 const pool = new Pool({connectionString: process.env.DATABASE_URL || 'postgresql://postgres:secret@localhost:5432/postgres'})
 
+const createTable = "CREATE TABLE IF NOT EXISTS person (id serial, first_name varchar(255), last_name varchar(255), eye_color varchar(255))"
+const insertRow = "INSERT INTO person (first_name, last_name, eye_color) VALUES ($1, $2, $3)"
+const updateEyeColor = "UPDATE person SET eye_color=($1) WHERE eye_color=($2)"
+const selectByFirstName = "SELECT * FROM person WHERE first_name = ($1)"
+
 pool.connect()
-  .then(() => console.log('Connected to Postgres!'))
-  .then(() => pool.query("CREATE TABLE IF NOT EXISTS person (id serial, first_name varchar(255), last_name varchar(255), eye_color varchar(255))"))
-  .then(() => {
-    const insertPerson = "INSERT INTO person (first_name, last_name, eye_color) VALUES ($1, $2, $3)"
-    pool.query(insertPerson, ['James', 'Smith', 'brown'])
-    pool.query(insertPerson, ['Frank', 'Jones', 'brown'])
-    pool.query(insertPerson, ['Rebecca', 'Andrews', 'blue'])
-    console.log('Inserted 3 people')
-  })
+  .then(() => pool.query(createTable))
+  .then(() => pool.query(insertRow, ['James', 'Smith', 'brown']))
+  .then(() => pool.query(insertRow, ['Frank', 'Jones', 'brown']))
+  .then(() => pool.query(insertRow, ['Rebecca', 'Andrews', 'blue']))
+  .then(() => pool.query(updateEyeColor, ['blue', 'brown']))
+  .then(() => pool.query(selectByFirstName, ['James']))
+  .then(res => console.log(res.rows))
   .catch(err => console.error(err))
 
